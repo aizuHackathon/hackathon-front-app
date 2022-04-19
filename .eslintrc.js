fd939866a5f16533002a2192f69279e2c323e4fb
Keisuke-Toku-2000 <keisuke20000923@gmail.com>
2022-04-19 19:19:40 +0900

    eslint 変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,29 +4,30 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
+    ecmaVersion: 12,
     sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint', 'react-hooks'],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
     indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    'react/display-name': 0,
-    'react/prop-types': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
